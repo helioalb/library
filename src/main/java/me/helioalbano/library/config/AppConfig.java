619d845effb62fffbb2726697771d4295a1516b5
Helio Albano de Oliveira <helioalb@gmail.com>
2023-10-03 10:55:59 -0300

    Add bean config to author service
    LibraryIssue-5

--- src/main/java/me/helioalbano/library/config/AppConfig.java
+++ src/main/java/me/helioalbano/library/config/AppConfig.java
@@ -3,8 +3,11 @@ package me.helioalbano.library.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import me.helioalbano.library.repository.AuthorRepository;
 import me.helioalbano.library.repository.BookRepository;
+import me.helioalbano.library.repository.implementation.AuthorRepositoryInMemory;
 import me.helioalbano.library.repository.implementation.BookRepositoryInMemory;
+import me.helioalbano.library.service.AuthorService;
 import me.helioalbano.library.service.BookService;
 
 @Configuration
@@ -16,5 +19,14 @@ public class AppConfig {
     @Bean
     BookService bookService() {
         return new BookService(bookRepository());
+    }
+
+    AuthorRepository authorRepository() {
+        return new AuthorRepositoryInMemory();
+    }
+
+    @Bean
+    AuthorService authorService() {
+        return new AuthorService(authorRepository());
     }
 }
